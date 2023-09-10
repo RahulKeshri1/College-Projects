4bcb654ce2652c29b6bf20e468844f363e8e3754
Rahul <rahulkumarkeshri475@gmail.com>
2023-09-10 23:51:37 +0530

    A Program to Define 2D Array and List.

---
+++ Python/2D List and Array.py
@@ -0,0 +1,8 @@
+import numpy as np
+row=int(input("Enter No. of Rows:\t"))
+col=int(input("Enter No. of Columns:\t"))
+list1=[[0]*col for i in range(row)]
+print(list1)
+
+arr=np.zeros((row,col))
+print(arr)
--- Python/2DArray.py
+++
@@ -1,4 +0,0 @@
-row=int(input("enter rows"))
-col=int(input("enter cols"))
-list1=[[0]*row for i in range(col)]
-print(list1)

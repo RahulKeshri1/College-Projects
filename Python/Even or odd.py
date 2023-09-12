017c6d7a5484112bf9cd6427150de709bd0235cb
Rahul <rahulkumarkeshri475@gmail.com>
2023-09-12 23:16:14 +0530

    To Check the No. is Even or Odd.

---
+++ Python/Even or odd.py
@@ -0,0 +1,10 @@
+def evod(num):
+    if num%2==0:
+        print("This number is Even.")
+    else:
+        print("This number is Odd.")
+def main():
+    a=int(input("Enter the number.\t"))
+    evod(a)
+
+main()

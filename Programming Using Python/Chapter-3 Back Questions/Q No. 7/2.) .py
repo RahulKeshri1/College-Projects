2768b9d4b44fc4e791dca2fec93b821708770b29
Rahul <rahulkumarkeshri475@gmail.com>
2023-10-07 22:53:56 +0530

    Q No. 2 of Chapter 3.

---
+++ Programming Using Python/Chapter-3 Back Questions/Q No. 7/2.) .py
@@ -0,0 +1,31 @@
+#Wr ite a function that finds the sum of the n terms of
+#the following series:
+#2. e^x=1 + x^2 /1! + x^2 /2! + x^3 /3! + … x^n /n!
+
+def factorial(n):
+    ans=1
+    if n==0 or n==1:
+        return ans
+    for i in range(1,n+1):
+        ans=ans*i
+
+    return ans
+
+
+def summation(x):
+    ans=1
+    n=1000
+    for i in range(1,n):
+            ans=ans+(pow(x,i))/(factorial(i))
+    
+    return ans
+import math
+
+def main():
+     x=int(input("Enter the power of e:\t"))
+     ans=summation(x)
+     print(f"Value of e^{x}={ans}")
+     print(f"Value of e^{x}={math.exp(x)}")
+     return ""
+
+main()

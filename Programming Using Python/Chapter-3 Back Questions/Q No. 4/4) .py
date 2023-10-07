5dc39b699c0a194f8ca205a672bec9ebd8d3b710
Rahul <rahulkumarkeshri475@gmail.com>
2023-10-07 22:53:24 +0530

    Q No. 5 of Chapter 3.

---
+++ Programming Using Python/Chapter-3 Back Questions/Q No. 4/4) .py
@@ -0,0 +1,18 @@
+# Write a function that takes two numbers as input
+#parameters and returns their least common m ultiple.
+
+def lcm(num1,num2):
+    for i in range(num1,num1*num2):
+        if i%num1==0 and i%num2==0:
+            return i
+    
+    return num1*num2
+
+def main():
+    s=int(input("Enter the First number:\t"))
+    r=int(input("Enter the Second number:\t"))
+    ans=lcm(s,r)
+    print(f"The LCM of {s} and {r}:\t",ans)
+    input()
+
+main()
